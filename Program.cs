6b3d1bd1f1af94eea7ede6cf91b00f891c970ab8
Primoz Susa <primoz.susa@pm.me>
2024-04-23 22:09:44 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Runtime.InteropServices;
+namespace CppBind
+{
+  class Program
+  {
+    [DllImport(@"./cpp/libhello-cpp.so")]
+    public static extern void PrintHelloWorld();
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, World C#!");
+      PrintHelloWorld();
+    }
+  }
+}
---
+++ cpp/HelloWorld.cpp
@@ -0,0 +1,7 @@
+#include "HelloWorld.h"
+#include <iostream>
+
+void PrintHelloWorld()
+{
+  std::cout << "Hello World!" << std::endl;
+}
